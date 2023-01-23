d43b33bffd3824d460f79862c9f038938869eb95
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2023-01-23 11:47:05 -0300

    alterei o conteudo de uma variavel

--- Logica/variable.dart
+++ Logica/variable.dart
@@ -1,6 +1,8 @@
 var primeira_variavel; //ma pratica no dart
-var primeiraVariavel = "Hello World"; //certo
+var primeiraVariavel = "Hello World1"; //certo
 
 void main() {
+  print(primeiraVariavel);
+  primeiraVariavel = "hello world";
   print(primeiraVariavel);
 }
