2fcedf7973fd083c93bbda7e1aa5a10bcd75acec
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2023-01-23 13:48:09 -0300

    bool
    brincando um pouco mais com o conceito de booleano

--- Logica/bool.dart
+++ Logica/bool.dart
@@ -3,4 +3,10 @@ void main() {
   final b = "DEV";
   final teste = a == b;
   print(teste);
+
+  //posso tambem negar o valor da variavel com o ! antes da variavel como no exemplo abaixo
+  final isFalse = false;
+  if (!isFalse) {
+    print("Entrou");
+  }
 }
